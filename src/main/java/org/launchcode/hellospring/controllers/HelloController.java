02d2663854655afe6ed69845a82ad3622021552a
Sean Duffy <kqrpnb@gmail.com>
2022-08-31 19:30:16 -0400

    mid-exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    // Handles /hello?name=LaunchCode requests
+//     Handles /hello?name=LaunchCode requests
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
@@ -37,16 +37,49 @@ public class HelloController {
     public String helloWithPathParam(@PathVariable String name) { return "Hello, " + name + "!";}
 
     // /hello/form because of @RequestMapping at @Controller level
-    @GetMapping("form")
+    @GetMapping("form2")
     public String helloForm(){
         return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
+                    "<body>" +
+                        "<form action='hello' method='post'>" + //submit a request to /hello
+                            "<input type='text' name='name'>" +
+                            "<input type='submit' value='Greet me!'>" +
+                        "</form>" +
+                    "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(value="form2", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+//        return "<h2>" + greeting + " " + n + "</h2>";
+
+        return greeting + " " + n;
     }
 
 }
